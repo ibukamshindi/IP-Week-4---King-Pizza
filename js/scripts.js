3b084261fd8932d1358cf6f8880238b9eeccd850
ibukamshindi <ibukamshindi@gmail.com>
2020-08-02 21:50:20 +0300

    add order column

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 var price, crust_price, topping_price ;
 let total=0;
 function eatpizza(size,crust,topping,total){
-  this.size = size 
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
 }
+  $(document).ready(function(){
+    $("submit.Checkout").click(function(event){
+      let
+
+  });
